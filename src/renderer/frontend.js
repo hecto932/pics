a2367fae683e62aeea5aeace9f591320449dbcbf
Hector Flores <hflores@nearsoft.comm>
2019-06-09 22:35:31 -0700

    Select an image

--- src/renderer/frontend.js
+++ src/renderer/frontend.js
@@ -1,5 +1,21 @@
 const os = require('os')
 
 window.addEventListener('load', () => {
-  console.log(os.cpus())
+  addImagesEvents()
 })
+
+function addImagesEvents () {
+  const thumbs = document.querySelectorAll('li.list-group-item')
+
+  for (let i = 0; i < thumbs.length; ++i) {
+    thumbs[i].addEventListener('click', function () {
+      changeImage(this)
+    })
+  }
+}
+
+function changeImage (node) {
+  document.querySelector('li.selected').classList.remove('selected')
+  node.classList.add('selected')
+  document.getElementById('image-displayed').src = node.querySelector('img').src
+}
