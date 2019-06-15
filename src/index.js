95ffc9e0e792848fbf9170379694d375189f7847
Hector Flores <hflores@nearsoft.comm>
2019-06-14 19:10:27 -0700

    Read directory files

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
-"use strict";
+'use strict';
 
-const debug = require("debug")("electron-app:index");
-const path = require("path");
-const { app, BrowserWindow, ipcMain, dialog } = require("electron");
+const debug = require('debug')('electron-app:index');
+const fs = require('fs');
+const path = require('path');
+const { app, BrowserWindow, ipcMain, dialog } = require('electron');
+const isImage = require('is-image');
+const filesize = require('filesize')
 
 // Adds debug features like hotkeys for triggering dev tools and reload
-require("electron-debug")();
+require('electron-debug')();
 
 // Prevent window being garbage collected
 let mainWindow;
@@ -25,37 +28,52 @@ function createMainWindow() {
     }
   });
 
-  win.loadURL(`file:///${path.join(__dirname, "renderer", "index.html")}`);
-  win.on("closed", onClosed);
+  win.loadURL(`file:///${path.join(__dirname, 'renderer', 'index.html')}`);
+  win.on('closed', onClosed);
 
   return win;
 }
 
-app.on("window-all-closed", () => {
+app.on('window-all-closed', () => {
   debug(`window-all-closed...`);
-  if (process.platform !== "darwin") {
+  if (process.platform !== 'darwin') {
     app.quit();
   }
 });
 
-app.on("activate", () => {
+app.on('activate', () => {
   debug(`activate`);
   if (!mainWindow) {
     mainWindow = createMainWindow();
   }
 });
 
-app.on("ready", () => {
+app.on('ready', () => {
   debug(`App is ready...`);
   mainWindow = createMainWindow();
 });
 
-ipcMain.on("open-directory", event => {
-  dialog.showOpenDialog(mainWindow, {
-    title: 'Select new location',
-    buttonLabel: 'open location',
-    properties: ['openDirectory']
-  }, (dir) => {
-    console.log(dir)
-  })
+ipcMain.on('open-directory', event => {
+  dialog.showOpenDialog(
+    mainWindow,
+    {
+      title: 'Select new location',
+      buttonLabel: 'open location',
+      properties: ['openDirectory']
+    },
+    dir => {
+      let images = [];
+      if (dir && dir.length) {
+        fs.readdir(dir[0], (err, files) => {
+          images = files
+            .filter(img => isImage(img))
+            .map(img => ({
+              src: `file://${path.join(dir[0], img)}`,
+              size: filesize(fs.statSync(path.join(dir[0], img)).size)
+            }));
+          console.log(images);
+        });
+      }
+    }
+  );
 });
